9983e6361adae6798e8cde944671a375bfd53234
matetakiese <israelmateta685@gmail.com>
2023-12-06 13:16:59 +0000

    init project

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import HeaderHome from '../components/header.jsx';
+import Happening from '../components/editor.jsx';
+ import TweetsMain from '../components/tweet.jsx';
+ import TheNewYorkTimes from '../components/tweetbody.jsx';
+import TweetCommit from '../components/tweetcommit.jsx';
+import TweetCommitTwo from '../components/tweetcommittwo.jsx';
+import TweetBodyTwo from '../components/tweetbodytwo.jsx';
 
 function Home() {
-    return (
-        <main className="timeline">
-            <h1>Timeline here</h1>
-        </main>
 
-    );
+  return (
+    <main className="timeline">
+      <HeaderHome />
+      <Happening />
+      <TweetsMain />
+      <TheNewYorkTimes />
+      <TweetCommit />
+      <TweetCommitTwo />
+      <TweetBodyTwo />
+    </main>
+  );
 }
 
 export default Home;
